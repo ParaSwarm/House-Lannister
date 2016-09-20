4b04f44f5399ffb9a0daf94508b1d48d6f3ab4a1
Gordon Klundt <gklundt@gmail.com>
2016-09-20 11:40:43 -0500

    Updating Service Model and Proxy definitions and adding comments.

--- app/src/main/java/edu/uco/houselannister/saveasingle/model/IUserProfile.java
+++ app/src/main/java/edu/uco/houselannister/saveasingle/model/IUserProfile.java
@@ -2,6 +2,8 @@ package edu.uco.houselannister.saveasingle.model;
 
 /**
  * Created by Gordon on 9/20/2016.
+ * This interface provides access methods
+ * to the users profile
  */
 public interface IUserProfile {
 
