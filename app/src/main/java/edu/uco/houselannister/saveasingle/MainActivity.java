5358482549a4f2f1531e45ba34f0e9ec8314b8c2
conyacr <conyacr@gmail.com>
2016-09-19 20:27:32 -0500

    Test navigation drawer
    Just was getting it working for the navigation drawer and how it would look/work there are a couple of changes for kitkit compared to marshmallow where the "hamburger" button doesn't yet exist in the kitkat envrionment.

--- app/src/main/java/edu/uco/houselannister/saveasingle/MainActivity.java
+++ app/src/main/java/edu/uco/houselannister/saveasingle/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.uco.houselannister.saveasingle;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -9,8 +16,12 @@ import butterknife.ButterKnife;
 import edu.uco.houselannister.saveasingle.model.ZipCode;
 
 public class MainActivity extends Activity {
+    private String[] navigationTitle;
+    private DrawerLayout navigationDrawer;
+    private ListView navigationDrawerListView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
-    @BindView(R.id.sample_TextView) TextView mTextView;
+//    @BindView(R.id.sample_TextView) TextView mTextView;
 
     public String something(){
         ZipCode x = new ZipCode("83838",23.34343,-38.333,"Edmond, OK");
@@ -20,10 +31,82 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
         //this is master!
 
         //mTextView = (TextView) findViewById(R.id.sample_TextView);
-        mTextView.setText(something());
+//        mTextView.setText(something());
+
+        //navigation drawer
+        navigationTitle = getResources().getStringArray(R.array.navigation_titles);
+        navigationDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationDrawerListView = (ListView) findViewById(R.id.left_drawer);
+        //set the adapter for the list view to use
+        navigationDrawerListView.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, navigationTitle));
+
+        navigationDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, navigationDrawer, R.string.open, R.string.closed) {
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            public void onDrawerClosed(View view) {
+
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        navigationDrawer.setDrawerListener(mDrawerToggle);
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+//        getActionBar().setHomeButtonEnabled(true);
+//        mDrawerToggle = new ActionBarDrawerToggle(
+//                this,                  /* host Activity */
+//                navigationDrawer,
+//                R.string.open,  /* "open drawer" description */
+//                R.string.closed  /* "close drawer" description */
+//        ) {
+//
+//            /** Called when a drawer has settled in a completely closed state. */
+//            public void onDrawerClosed(View view) {
+//                super.onDrawerClosed(view);
+//                getActionBar().setTitle("closed");
+//            }
+//
+//            /** Called when a drawer has settled in a completely open state. */
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle("open");
+//            }
+//        };
+//
+//        // Set the drawer toggle as the DrawerListener
+//        navigationDrawer.setDrawerListener(mDrawerToggle);
+
+//        getActionBar().setDisplayHomeAsUpEnabled(true);
+//        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 }
