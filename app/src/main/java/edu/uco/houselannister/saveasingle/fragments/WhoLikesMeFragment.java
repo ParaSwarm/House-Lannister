60c97ee61d9b5bb61fdb762ac378dff08a760448
Samuel <songssam@gmail.com>
2016-10-11 10:07:20 -0500

    adding and removing, favroiteList, and sharing my private album

--- app/src/main/java/edu/uco/houselannister/saveasingle/fragments/WhoLikesMeFragment.java
+++ app/src/main/java/edu/uco/houselannister/saveasingle/fragments/WhoLikesMeFragment.java
@@ -31,13 +31,12 @@ public class WhoLikesMeFragment extends ListFragment implements OnItemClickListe
 
     private static final String KEY_MOVIE_TITLE = "key_title";
     ActionMode mMode;
-    ActionMode.Callback mCallback;
-    CharSequence[] array = {"Add to My Likes", "Share My Private Album", "Block"};
+    CharSequence[] array = {"Add to my favorites", "Share my private album", "Block"};
     private int pos;
     ArrayList<User> FavoritesArrayList;
     ArrayList<User> BlockArrayList;
-    ArrayList<String> StringFavoritesArrayList = new ArrayList<String>();
-    private String test;
+    ArrayList<User> AccessPrivatePhotoList;
+    String photoMessage;
 
     public WhoLikesMeFragment() {
         // Required empty public constructor
@@ -51,17 +50,16 @@ public class WhoLikesMeFragment extends ListFragment implements OnItemClickListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         appModel = AppModel.getAppModelInstance(AppService.getAppServiceInstance());
-        for (User u : appModel.getCurrentUser().getInteractions().getFavorites()) {
-            StringFavoritesArrayList.add(u.getName() + " - " + u.getEmailAddress());
-        }
-        if (appModel.getCurrentUser().getInteractions().getFavorites().size() == 0)
+
+        if (appModel.getCurrentUser().getInteractions().getFavorites() == null)
             FavoritesArrayList = new ArrayList<User>();
         else
             FavoritesArrayList = new ArrayList<User>(appModel.getCurrentUser().getInteractions().getFavorites());
-        if (appModel.getCurrentUser().getInteractions().getBlocked() == null)
-            BlockArrayList = new ArrayList<User>();
+
+        if (appModel.getCurrentUser().getInteractions().getPrivatePhotoAccess() == null)
+            AccessPrivatePhotoList = new ArrayList<User>();
         else
-            BlockArrayList = new ArrayList<User>(appModel.getCurrentUser().getInteractions().getBlocked());
+            AccessPrivatePhotoList = new ArrayList<User>(appModel.getCurrentUser().getInteractions().getPrivatePhotoAccess());
     }
 
     @Override
@@ -84,34 +82,62 @@ public class WhoLikesMeFragment extends ListFragment implements OnItemClickListe
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 mMode = null;
                 pos = position;
+                boolean favoriteCheck = false;
+                boolean photoCheck = false;
+                for (User u : FavoritesArrayList) {
+                    if (u.getEmailAddress().contains(appModel.getUsers().get(pos).getEmailAddress())) {
+                        favoriteCheck = true;
+                    }
+                }
+                if (favoriteCheck) {
+                    array[0] = "Remove from my favorites.";
+                    photoMessage = "Do you want to stop sharing the private album with ";
+                } else {
+                    array[0] = "Add to my favorites.";
+                }
+
+                for (User u : AccessPrivatePhotoList) {
+                    if (appModel.getUsers().get(pos).getEmailAddress().equals(u.getEmailAddress())) {
+                        photoCheck = true;
+                    }
+                }
+                if (photoCheck) {
+                    array[1] = "Stop sharing my private album.";
+                    photoMessage = "Do you want to stop sharing the private album with ";
+                } else {
+                    array[1] = "Share my private album.";
+                    photoMessage = "Do you want to share the private album with ";
+                }
+
+
                 new AlertDialog.Builder(getActivity())
                         .setTitle(appModel.getUsers().get(position).getName())
                         .setItems(array, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 switch (which) {
                                     case 0:
-                                        boolean found = false;
-                                        for (User u : appModel.getCurrentUser().getInteractions().getFavorites()) {
-                                            if (appModel.getUsers().get(pos).getEmailAddress().equals(u.getEmailAddress())) {
-                                                Toast.makeText(getActivity(), appModel.getUsers().get(pos).getName() + " is already on the favorite list", Toast.LENGTH_SHORT).show();
-                                                found = true;
-                                                break;
-                                            } else {
-                                                found = false;
-                                            }
-                                        }
-                                        if (!found) {
+                                        if (array[0].toString().equals("Add to my favorites.")) {
                                             Toast.makeText(getActivity(), appModel.getUsers().get(pos).getName() + " is added to the favorite list", Toast.LENGTH_SHORT).show();
                                             FavoritesArrayList.add(appModel.getUsers().get(pos));
+                                            appModel.getCurrentUser().getInteractions().setFavorites(FavoritesArrayList);
+                                        } else {
+                                            Toast.makeText(getActivity(), appModel.getUsers().get(pos).getName() + " is removed from the favorite list", Toast.LENGTH_SHORT).show();
+                                            FavoritesArrayList.remove(FavoritesArrayList.indexOf(appModel.getUsers().get(pos)));
                                             appModel.getCurrentUser().getInteractions().setFavorites(FavoritesArrayList);
                                         }
                                         break;
                                     case 1:
                                         new AlertDialog.Builder(getActivity())
-                                                .setTitle("Do you want to share the private album with " + appModel.getUsers().get(pos).getName() + "?")
+                                                .setTitle(photoMessage + appModel.getUsers().get(pos).getName() + "?")
                                                 .setNegativeButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                                     public void onClick(DialogInterface dialog, int which) {
-                                                        // do nothing
+                                                        if (array[1].toString().equalsIgnoreCase("Stop sharing my private album.")) {
+                                                            AccessPrivatePhotoList.remove(AccessPrivatePhotoList.indexOf(appModel.getUsers().get(pos)));
+                                                            appModel.getCurrentUser().getInteractions().setFavorites(AccessPrivatePhotoList);
+                                                        } else {
+                                                            AccessPrivatePhotoList.add(appModel.getUsers().get(pos));
+                                                            appModel.getCurrentUser().getInteractions().setFavorites(AccessPrivatePhotoList);
+                                                        }
                                                     }
                                                 })
                                                 .setPositiveButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -126,29 +152,14 @@ public class WhoLikesMeFragment extends ListFragment implements OnItemClickListe
                                                 .setTitle("Do you want to block " + appModel.getUsers().get(pos).getName() + "?")
                                                 .setNegativeButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                                                     public void onClick(DialogInterface dialog, int which) {
-//                                                        boolean found = false;
-//                                                        for (User u : BlockArrayList) {
-//                                                            if (appModel.getUsers().get(pos).getEmailAddress().equals(u.getEmailAddress())) {
-//                                                                Toast.makeText(getActivity(), appModel.getUsers().get(pos).getName() + " is already on blocked list", Toast.LENGTH_SHORT).show();
-//                                                                found = true;
-//                                                                break;
-//                                                            } else {
-//                                                                found = false;
-//                                                            }
-//                                                        }
-//                                                        if (!found) {
-//                                                            Toast.makeText(getActivity(), appModel.getUsers().get(pos).getName() + " is added to blocked list", Toast.LENGTH_SHORT).show();
-//                                                            BlockArrayList.add(appModel.getUsers().get(pos));
-//                                                            appModel.getCurrentUser().getInteractions().setBlocked(BlockArrayList);
-//                                                        }
+//
                                                     }
                                                 })
                                                 .setPositiveButton(android.R.string.no, new DialogInterface.OnClickListener() {
                                                     public void onClick(DialogInterface dialog, int which) {
                                                         // do nothing
                                                     }
-                                                })
-                                                .show();
+                                                }).show();
                                         break;
                                 }
                             }
@@ -157,8 +168,7 @@ public class WhoLikesMeFragment extends ListFragment implements OnItemClickListe
                             public void onClick(DialogInterface dialog, int which) {
                                 // do nothing
                             }
-                        })
-                        .show();
+                        }).show();
                 return true;
             }
         });
